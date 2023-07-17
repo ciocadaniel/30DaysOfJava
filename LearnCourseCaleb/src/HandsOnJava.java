10de10ed8855c96d21ac6c948c0fb91ce77d98ca
Daniel Cioca <daniel.cioca76@gmail.com>
2023-07-18 01:37:46 +0300

    Read from File

--- LearnCourseCaleb/src/HandsOnJava.java
+++ LearnCourseCaleb/src/HandsOnJava.java
@@ -1,9 +1,32 @@
 
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class HandsOnJava {
 
-	public static void main(String[] args) {
-	System.out.println("hello");
+	public static void main(String[] args) throws Exception {
 
+		 
+   Scanner in = new Scanner(new File("D:\\JavaSavings\\test.txt"));
+   
+   List<String> names = new ArrayList<String>();
+   
+   while(in.hasNextLine())
+	   names.add(in.nextLine());
+   
+   for(String u : names)
+	   System.out.println(u);
+
+  in.close();	
+   
+	}
 	}
 
-}
+
+
+	
