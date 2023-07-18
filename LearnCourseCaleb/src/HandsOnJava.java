393d227819c741f7732122ad5139e886a1940dea
ciocadaniel <139668040+ciocadaniel@users.noreply.github.com>
2023-07-18 15:31:40 +0300

    Read from File two methods

--- LearnCourseCaleb/src/HandsOnJava.java
+++ LearnCourseCaleb/src/HandsOnJava.java
@@ -1,7 +1,9 @@
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,8 +12,10 @@ import java.util.Scanner;
 public class HandsOnJava {
 
 	public static void main(String[] args) throws Exception {
-
-		 
+		// Read from File
+		
+		
+		// Method 1 with Scanner
    Scanner in = new Scanner(new File("D:\\JavaSavings\\test.txt"));
    
    List<String> names = new ArrayList<String>();
@@ -23,6 +27,19 @@ public class HandsOnJava {
 	   System.out.println(u);
 
   in.close();	
+  
+     // Method 2 with BufferedReader Reader
+  
+  File file = new File("D:\\JavaSavings\\test.txt");
+  
+  BufferedReader br = new BufferedReader(new FileReader(file));
+  
+  String st;
+  while((st=br.readLine())!=null)
+	  names.add(st);
+  
+  for(String u : names)
+	  System.out.println(u+" nou");
    
 	}
 	}
