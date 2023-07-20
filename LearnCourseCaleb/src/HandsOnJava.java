fa5dd6f2b3bd2034475f465fdf9588c037ab2692
ciocadaniel <139668040+ciocadaniel@users.noreply.github.com>
2023-07-20 03:33:46 +0300

    Stack Example

--- LearnCourseCaleb/src/HandsOnJava.java
+++ LearnCourseCaleb/src/HandsOnJava.java
@@ -1,46 +1,36 @@
 
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 
 public class HandsOnJava {
 
-	public static void main(String[] args) throws Exception {
-		// Read from File
-		
-		
-		// Method 1 with Scanner
-   Scanner in = new Scanner(new File("D:\\JavaSavings\\test.txt"));
-   
-   List<String> names = new ArrayList<String>();
-   
-   while(in.hasNextLine())
-	   names.add(in.nextLine());
-   
-   for(String u : names)
-	   System.out.println(u);
+	public static void main(String[] args) throws FileNotFoundException {
 
-  in.close();	
-  
-     // Method 2 with BufferedReader Reader
-  
-  File file = new File("D:\\JavaSavings\\test.txt");
-  
-  BufferedReader br = new BufferedReader(new FileReader(file));
-  
-  String st;
-  while((st=br.readLine())!=null)
-	  names.add(st);
-  
-  for(String u : names)
-	  System.out.println(u+" nou");
-   
+		LinkedList<String> names = new LinkedList<>();
+	   names.push("Darius");
+	   names.push("Adela");
+	   names.push("Alex");
+	   names.push("Carmen");
+	   
+	   System.out.println(names.pop());
+	   System.out.println(names.pop());
+	   System.out.println(names.pop());
+	   System.out.println(names.pop());
+	  
+
+		
+	  
+	
+	   
+	
+		
+		
+		
 	}
 	}
 
