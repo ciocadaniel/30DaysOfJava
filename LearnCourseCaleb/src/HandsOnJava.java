814aac6c2c844d4f1ff3ebf41fe864719e5202a2
ciocadaniel <139668040+ciocadaniel@users.noreply.github.com>
2023-07-20 04:11:59 +0300

    Iterators

--- LearnCourseCaleb/src/HandsOnJava.java
+++ LearnCourseCaleb/src/HandsOnJava.java
@@ -2,7 +2,9 @@
 
 import java.io.FileNotFoundException;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.io.File;
 
 
@@ -10,21 +12,42 @@ import java.io.File;
 public class HandsOnJava {
 
 	public static void main(String[] args) throws FileNotFoundException {
-
+  
+	
 		LinkedList<String> names = new LinkedList<>();
 	   names.push("Darius");
 	   names.push("Adela");
 	   names.push("Alex");
 	   names.push("Carmen");
+	
+	  
+	   Iterator<String> it = names.iterator();
+	/* 
+	  System.out.println(it.next());
+	  System.out.println(it.next());
+	  System.out.println(it.next());
+	  System.out.println(it.next());
+	 */ 
 	   
-	   System.out.println(names.pop());
-	   System.out.println(names.pop());
-	   System.out.println(names.pop());
-	   System.out.println(names.pop());
+	   
+	//  while(it.hasNext())
+		//  System.out.println(it.next());
 	  
+	   
+	   names.add(2,"Adrian");
 
+ 
+
+		ListIterator<String> is = names.listIterator();
 		
-	  
+		is.next();
+		is.next();
+		is.next();
+		
+		is.add("Karate");
+		
+	     for(String s : names)
+	    	 System.out.println(s); 
 	
 	   
 	
